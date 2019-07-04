585518f220f7f14fe0243f6334ce67c02783a8d3
Tim Sherry <tsherry82@gmail.com>
2019-07-03 21:03:46 -0400

    added var and starting to add functions

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+// ***********PSUEDOCODE************
+//*************Variables*************
+var targetNumber = [Math.floor(Math.random() * 101)+ 19];
+
+
+// **********Score***********
+var counter = 0
+
+var wins = 0
+var losses = 0
+
+
+// ******************Stones***************
+var thunderStone = [Math.floor(Math.random() * 10) + 1];
+var fireStone = [Math.floor(Math.random() * 10) + 1];
+var leafStone = [Math.floor(Math.random() * 10) + 1];
+var moonStone = [Math.floor(Math.random() * 10) + 1];
+
+// *************Functions***************
+
+// *************
